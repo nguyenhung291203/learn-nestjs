203928b3eace1bb9a0317935a10d38d440a89b75
Hung Nguyen <hung.nguyen@piscada.com>
2025-12-19 13:11:30 +0700

    add HashingService

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { Global, Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { HashingService } from './hashing/hashing.service'
 
-const shared = [PrismaService]
+const shared = [PrismaService, HashingService]
 @Global()
 @Module({
 	providers: shared,
